c4984f7c1bb1372ea771e131de681e862f1c483d
zzzMini <135273040+zzzmini@users.noreply.github.com>
2024-03-13 18:00:12 +0900

    quiz_add

--- Java/javawork/javastudy/ch06/quiz/LottoNum.java
+++ Java/javawork/javastudy/ch06/quiz/LottoNum.java
@@ -4,6 +4,26 @@ import java.util.Random;
 
 public class LottoNum {
     public static void main(String[] args) {
-        
+        Random random = new Random();
+        int lottoNum[] = new int[6];
+        int currNum;
+        int index = 0;
+        boolean flag = false;
+        do{
+            currNum = random.nextInt(45)+1;
+            for(int i=0; i<lottoNum.length; i++){
+                if(currNum == lottoNum[i]){
+                    flag = true;
+                    break;
+                }
+            }
+            if(flag == false){
+                lottoNum[index] = currNum;
+                index = index + 1;
+            }
+        } while (index <= 5);
+        for(int result : lottoNum){
+            System.out.print(result + " ");
+        }
     }
 }
