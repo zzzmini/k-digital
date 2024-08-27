e8f970dcbdb025f76f05bbbb18d1d3c4f6b87823
zzzMini <135273040+zzzmini@users.noreply.github.com>
2024-08-27 15:45:31 +0900

    Create filterBox.vue

---
+++ Vue/stargram/filterBox.vue
@@ -0,0 +1,42 @@
+<template>
+  <div 
+  @click="changeFilter"
+  :class="`${filter} filter-item`"
+  :style="{backgroundImage : `url(${strUrl})`}">
+        <slot></slot>
+        <!-- <button @click="fire">발사</button> -->
+  </div>
+</template>
+
+<script>
+export default {
+    name : 'FilterBox',
+    props :{
+        strUrl : String,
+        filter : String,
+    },
+    methods: {
+        changeFilter(){
+            this.emitter.emit('changeFilter', this.filter);
+        },
+        fire(){
+            this.emitter.emit('fire', 'FilterBox에서 쏨');
+        }
+    },
+}
+</script>
+
+<style>
+@import '../assets/css/cssgram.min.css';
+
+    .filter-item {
+        width: 100px;
+        height: 100px;
+        margin: 10px 10px 10px auto;
+        padding: 8px;
+        color: white;
+        display: inline-block;
+        background-size: cover;
+        background-position: center;
+    }
+</style>
