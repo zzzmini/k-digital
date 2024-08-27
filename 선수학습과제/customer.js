1d3194918f1260328a05f727eafffc8a49ef584e
zzzMini <135273040+zzzmini@users.noreply.github.com>
2024-08-27 10:50:43 +0900

    Add files via upload

---
+++ 선수학습과제/customer.js
@@ -0,0 +1,90 @@
+export default[
+    {
+        cus_id : 1,
+        cus_name: "윤하나",
+        jumin_id: "700110-1324561",
+        email: "khg@gmail.com",
+        tel: "02-100-2000",
+        phone : "010-1200-2300",
+        job: "회사원",
+        addr: "서울특별시 종로구 서대문로 123-3",
+        m_id : 1
+    },
+    {
+        cus_id : 2,
+        cus_name: "강해라",
+        jumin_id: "780120-2324521",
+        email: "kkka@naver.com",
+        tel: "032-700-1000",
+        phone : "010-5900-7211",
+        job: "자영업",
+        addr: "서울특별시 마포구 홍은로 100",
+        m_id : 2
+    },
+    {
+        cus_id : 3,
+        cus_name: "나란다",
+        jumin_id: "931125-1015261",
+        email: "naranda@daum.net",
+        tel: "02-587-1234",
+        phone : "010-7700-9981",
+        job: "회사원",
+        addr: "서울특별시 강남구 봉은사로 751",
+        m_id : 1
+    },
+    {
+        cus_id : 4,
+        cus_name: "담나라",
+        jumin_id: "000110-4324561",
+        email: "nara@gmail.com",
+        tel: "02-555-6666",
+        phone : "010-9221-4150",
+        job: "전문직",
+        addr: "서울특별시 강서구 공항동 3",
+        m_id : 3
+    },
+    {
+        cus_id : 5,
+        cus_name: "라면다",
+        jumin_id: "851027-1000561",
+        email: "noodleok@naver.com",
+        tel: "032-235-4567",
+        phone : "010-7899-1234",
+        job: "회사원",
+        addr: "인천광역시 남동구 소래로 500",
+        m_id : 1
+    },        
+    {
+        cus_id : 6,
+        cus_name: "박석일",
+        jumin_id: "791110-2233561",
+        email: "packstone@daum.net",
+        tel: "032-762-9800",
+        phone : "010-1312-5600",
+        job: "회사원",
+        addr: "서울특별시 종로구 서대문로 123-3",
+        m_id : 2
+    },    
+    {
+        cus_id : 7,
+        cus_name: "이성경",
+        jumin_id: "030110-1324561",
+        email: "leebible@yahoo.com",
+        tel: "000-0000-0000",
+        phone : "010-1122-2333",
+        job: "학생",
+        addr: "경기도 일산동구 일산로 1",
+        m_id : 3
+    },    
+    {
+        cus_id : 8,
+        cus_name: "김임시",
+        jumin_id: "123456-1234567",
+        email: "izoin@naver.com",
+        tel: "010-0000-0000",
+        phone : "010-0000-0000",
+        job: "모름",
+        addr: "",
+        m_id : 0
+    },        
+  ]
---
+++ 선수학습과제/manager.js
@@ -0,0 +1,22 @@
+export default[
+    {
+        m_id : 1,
+        m_name: "강하게",
+        dept : "관리2팀",
+        position: "사원",
+        phone : "010-2300-4100",
+    },    {
+        m_id : 2,
+        m_name: "나대리",
+        dept : "관리2팀",
+        position: "대리",
+        phone : "010-1100-5555",
+    },
+    {
+        m_id : 3,
+        m_name: "고과장",
+        dept : "관리2팀",
+        position: "주임",
+        phone : "010-2236-5122",
+    },
+  ]
