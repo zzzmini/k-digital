0111498384de548f3574fe92d45e2007eeeebf8e
zzzMini <135273040+zzzmini@users.noreply.github.com>
2024-08-30 08:54:28 +0900

    Update professor_data.sql

--- Mysql/professor_data.sql
+++ Mysql/professor_data.sql
@@ -1,19 +1,29 @@
-INSERT INTO professor
-(name, belong, phone)
-VALUES('유재석', 'IDE','01012345678');
+DROP TABLE if exists professor;
+CREATE TABLE professor
+(
+     p_id INT AUTO_INCREMENT,
+     name VARCHAR(32) NOT NULL,
+     belong VARCHAR(20) DEFAULT 'FOO',
+     phone VARCHAR(20),
+     PRIMARY KEY(p_id)
+);
 
 INSERT INTO professor
 (name, belong, phone)
-VALUES('황영조', 'MSE', '01021342443');
+VALUES('유재석', 'COMPUTER','01012345678');
 
 INSERT INTO professor
 (name, belong, phone)
-VALUES('케이멀', 'ESE', '01023424343');
-
-INSERT INTO professor
-(_id, name, belong, phone)
-VALUES(256, '호날두', 'IME', '01034343222');
+VALUES('황영조', 'SPORTS', '01021342443');
 
 INSERT INTO professor
 (name, belong, phone)
-VALUES( '리오넬', 'IDE', '01023432432');
+VALUES('케이멀', 'DANCE', '01023424343');
+
+INSERT INTO professor
+(name, belong, phone)
+VALUES('호날두', 'ENGLISH', '01034343222');
+
+INSERT INTO professor
+(name, belong, phone)
+VALUES('김형민','COMPUTER', '01023432432');
