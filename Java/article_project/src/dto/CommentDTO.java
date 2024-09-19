184b42dbf22a615a42801ac604f78dcb145ab785
zzzMini <135273040+zzzmini@users.noreply.github.com>
2024-09-19 15:54:54 +0900

    Add files via upload

---
+++ Java/article_project/src/dto/CommentDTO.java
@@ -0,0 +1,52 @@
+package dto;
+
+import java.time.LocalDateTime;
+
+import common.CommonField;
+
+public class CommentDTO extends CommonField{
+	private Long comment_id;
+	private Long article_id ;
+	private String c_name;
+	private String c_content;
+	
+	public CommentDTO() {}
+	
+	
+	public CommentDTO(Long comment_id, Long article_id, 
+			String c_name, String c_content, LocalDateTime insertedDate,
+			LocalDateTime updatedDate) {
+		this.comment_id = comment_id;
+		this.article_id = article_id;
+		this.c_name = c_name;
+		this.c_content = c_content;
+		setInsertedDate(insertedDate);
+		setUpdatedDate(updatedDate);
+	}
+
+
+	public Long getComment_id() {
+		return comment_id;
+	}
+	public void setComment_id(Long comment_id) {
+		this.comment_id = comment_id;
+	}
+	public Long getArticle_id() {
+		return article_id;
+	}
+	public void setArticle_id(Long article_id) {
+		this.article_id = article_id;
+	}
+	public String getC_name() {
+		return c_name;
+	}
+	public void setC_name(String c_name) {
+		this.c_name = c_name;
+	}
+	public String getC_content() {
+		return c_content;
+	}
+	public void setC_content(String c_content) {
+		this.c_content = c_content;
+	}
+}
