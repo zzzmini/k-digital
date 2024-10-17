9eabe19b26151fc4298ab25a8fd0f47d926ed497
zzzMini <135273040+zzzmini@users.noreply.github.com>
2024-10-17 09:07:12 +0900

    Create blog.sql

---
+++ SpringBoot/myboard/blog.sql
@@ -0,0 +1,5 @@
+insert into blog (name, user_image, post_image,likes, date, liked, content,filter ) values
+('zzzMini', 'https://picsum.photos/100?random=1','https://picsum.photos/600?random=1',5, 'May 14', FALSE,
+'API 통해 요청한 게시물','aden'),
+('Mini', 'https://picsum.photos/100?random=2','https://picsum.photos/600?random=2',1, 'July 25', FALSE,
+ '서버에서 받은 게시물 두번째','perpetua');
