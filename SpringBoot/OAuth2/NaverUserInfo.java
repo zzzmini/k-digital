d432e0829d6f0ea7993e09e2a2ec9bf138315014
zzzMini <135273040+zzzmini@users.noreply.github.com>
2024-10-23 14:05:32 +0900

    Create NaverUserInfo.java

---
+++ SpringBoot/OAuth2/NaverUserInfo.java
@@ -0,0 +1,22 @@
+public class NaverUserInfo implements OAuth2UserInfo{
+    private Map<String, Object> attributes;
+    public NaverUserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+    @Override
+    public String getProviderId() {
+        return (String) attributes.get("id");
+    }
+    @Override
+    public String getProvider() {
+        return "naver";
+    }
+    @Override
+    public String getEmail() {
+        return (String) attributes.get("email");
+    }
+    @Override
+    public String getName() {
+        return (String) attributes.get("name");
+    }
+}
