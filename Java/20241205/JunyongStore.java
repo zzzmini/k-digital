fb4d6791368cf2a83afc2361b890fb7695a7cfc5
zzzMini <135273040+zzzmini@users.noreply.github.com>
2024-12-05 16:25:28 +0900

    Add files via upload

---
+++ Java/20241205/JunyongStore.java
@@ -0,0 +1,16 @@
+package basic.method;
+
+public class JunyongStore {
+    int minusNumbers(int numA, int numB) {
+        return numA - numB;
+    }
+
+    int addNumbers(int numA, int numB) {
+//        int sum = numA + numB;
+        return numA + numB;
+    }
+
+    void printTitle(String data) {
+        System.out.println(data);
+    }
+}
