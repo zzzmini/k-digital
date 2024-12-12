26a47baaad4178f0e36afd8c67438bc91b9f0918
zzzMini <135273040+zzzmini@users.noreply.github.com>
2024-12-12 15:13:37 +0900

    Add files via upload

---
+++ Java/20241212/src/view/AdminView.java
@@ -0,0 +1,7 @@
+package view;
+
+public class AdminView {
+    public void adminView() {
+
+    }
+}
---
+++ Java/20241212/src/view/UserView.java
@@ -0,0 +1,15 @@
+package view;
+
+public class UserView {
+    public void insertCoin() {
+        System.out.println("동전투입 화면");
+    }
+
+    public void returnMoney() {
+        System.out.println("잔돈반환 화면");
+    }
+
+    public void selectMenu() {
+        System.out.println("메뉴 선택 화면");
+    }
+}
