144021f907c8e0477e3638e09af05ce2ebba48bc
zzzMini <135273040+zzzmini@users.noreply.github.com>
2025-02-27 09:26:57 +0900

    Delete SpringBoot/myboard/project directory

--- SpringBoot/myboard/project/java/com/ex/myboard/repository/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ex.myboard.repository;
-
-import com.ex.myboard.entity.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleRepository  extends JpaRepository<Article, Long> {
-}
