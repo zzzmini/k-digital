b4c95f63cf7ee3f5b21648919bd0d92080bd9e95
zzzMini <135273040+zzzmini@users.noreply.github.com>
2025-04-30 17:01:55 +0900

    Create App.js

---
+++ OpenAI_LLM/chatbot/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Chatbot from "./Chatbot";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Chatbot</h1>
+      <Chatbot />
+    </div>
+  );
+};
+
+export default App;
