a34a21cd6514b1914c9e7150d52daeca4bfe956d
zzzMini <135273040+zzzmini@users.noreply.github.com>
2025-07-09 13:45:59 +0900

    Update professor_data.sql

--- Mysql/professor_data.sql
+++ Mysql/professor_data.sql
@@ -3,7 +3,7 @@ CREATE TABLE professor
 (
      p_id INT AUTO_INCREMENT,
      name VARCHAR(32) NOT NULL,
-     belong VARCHAR(20) DEFAULT 'FOO',
+     belong VARCHAR(20) DEFAULT 'COMPUTER',
      phone VARCHAR(20),
      PRIMARY KEY(p_id)
 );
@@ -14,11 +14,11 @@ VALUES('유재석', 'COMPUTER','01012345678');
 
 INSERT INTO professor
 (name, belong, phone)
-VALUES('황영조', 'SPORTS', '01021342443');
+VALUES('손흥민', 'SPORTS', '01021342443');
 
 INSERT INTO professor
 (name, belong, phone)
-VALUES('케이멀', 'DANCE', '01023424343');
+VALUES('장원영', 'DANCE', '01023424343');
 
 INSERT INTO professor
 (name, belong, phone)
